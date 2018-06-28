6dd3868b876d1807623928c1625f04a5837d6922
NickHu1234 <38758554+NickHu1234@users.noreply.github.com>
2018-06-28 12:48:56 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+﻿<?php
+/*
+$_POST返回数组值
+array (
+  'app' => 'live',
+  'flashver' => 'FMLE/3.0 (compatible; FMSc/1.0)',
+  'swfurl' => 'rtmp://192.168.0.115/live',
+  'tcurl' => 'rtmp://192.168.0.115/live',
+  'pageurl' => '',
+  'addr' => '192.168.0.199',
+  'clientid' => '79',
+  'call' => 'publish',
+  'name' => 'test3',
+  'type' => 'live',
+  'pass' => '123456',
+)
+*/
+$name = $_POST['name'];
+#$name = test3
+$pass = $_POST['pass'];
+$pass = 123456
+
+ if(empty($name) || empty($pass)){
+    echo "串码流不正确";
+    // 这个是thinkphp5的返回头信息的函数
+     header("HTTP/1.1 404 Not Found");  
+    header("Status: 404 Not Found");  
+    exit;  
+ }
+
+ echo "正常";
+?>
